bc0f54ab6cb84191dbc4778496e3edd1c6404bb9
Алексей <fortuk22@rambler.ru>
2022-05-13 01:38:32 +0300

    дз

--- src/App.js
+++ src/App.js
@@ -8,23 +8,25 @@ import TransactionHistory from './components/TransactionHistory/TransactionHisto
 import transactions from './data/transactions.json';
 
 function App() {
-  return (
-    <div className="App">
-      <Profile
-        name={user.name}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
+        return (
+            <div className="App">
+                <Profile
+                    name={user.name}
+                    tag={user.tag}
+                    location={user.location}
+                    avatar={user.avatar}
+                    stats={user.stats}
+                />
 
-      <Statistics title="Upload stats" stats={statisticalData} />
-      <Statistics stats={statisticalData} />
-      <FriendList friends={friends} />
+                <Statistics title="Upload stats" stats={statisticalData} />
+                <Statistics stats={statisticalData} />
+                <FriendList friends={friends} />
 
-      <TransactionHistory items={transactions} />
-    </div>
-  );
-}
+                <TransactionHistory items={transactions} />
+            </div>
+        );
+    }
+
+
 
 export default App;
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,21 +1,18 @@
 import s from './Statistics.module.css';
 import PropTypes from 'prop-types';
 
-const color = item => {
-  const colorArray = ['#E74C3C', '#8E44AD', '#3498DB', '#27AE60', '#F39C12'];
-  return `${colorArray[Number(item.id.replace(/[^0-9]/g, '')) % 5]}`;
-};
+
 
 const Statistics = ({ title, stats }) => {
   return (
     <section className={s.statistics}>
       <h2 class={s.title}>{title}</h2>
       <ul class={s.statList}>
-        {stats.map(({ id, label, percentage,})=> (
+        {stats.map(({ id, label, percentage, color }) => (
           <li
             key={id}
             class={s.item}
-            style={{ backgroundColor: color(item) }}
+            style={{ backgroundColor: color }}
           >
             <span class={s.label}>{label}</span>
             <span class={s.percentage}>{percentage}%</span>
@@ -30,10 +27,10 @@ Statistics.propTypes = {
   title: PropTypes.string,
   stats: PropTypes.arrayOf(
     PropTypes.shape({
-  id: PropTypes.string.isRequired,
-  label: PropTypes.string.isRequired,
-  percentage: PropTypes.number.isRequired,
-  }),
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    }),
   ).isRequired,
 };
 
